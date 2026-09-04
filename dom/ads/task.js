2b5c9aeb81c2f8022275b89585354fe4116ee6a4
Anton Ivanov <medik13anton@gmail.com>
2026-09-04 11:21:18 +0300

    смена каждую секунду

---
+++ dom/ads/task.js
@@ -0,0 +1,14 @@
+const rotators = document.querySelectorAll('.rotator');
+rotators.forEach(rotator => {
+    const cases = rotator.querySelectorAll('.rotator__case');
+    let currentIndex = 0;
+    setInterval(() => {
+        cases[currentIndex].classList.remove('rotator__case_active');
+        currentIndex++;
+        if (currentIndex >= cases.length) {
+            currentIndex = 0;
+        }
+        cases[currentIndex].classList.add('rotator__case_active');
+        
+    }, 1000); 
+});
